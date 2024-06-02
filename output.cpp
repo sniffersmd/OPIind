0faab031469b08049dd3cd93cd38431ae4d34871
sniffersmd <globalfrogender.com>
2024-06-02 23:29:41 +0300

    add output files

---
+++ output.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "output.h"
+#include <iostream>
+
+void displayResults(int total_teams) {
+    std::cout << "Общее количество команд: " << total_teams << std::endl;
+}
---
+++ output.h
@@ -0,0 +1,6 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+void displayResults(int total_teams);
+
+#endif // OUTPUT_H
